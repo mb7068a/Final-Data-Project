7b68acd33afc3c3ce29933fed6f201420411167b
Matthew <mb7068a@student.american.edu>
2019-12-06 16:34:03 -0500

    sdfg

--- Applied Data Science Project/Code/Citizens united.R
+++ Applied Data Science Project/Code/Citizens united.R
@@ -4,3 +4,4 @@ Senate5<-Senate5%>%
   filter(year<=2008)
 #Export Data
 write.table(Senate5, "~/Documents/GitHub/Final-Data-Project/Tables/Senate.csv", sep="\t")
+
--- Preformance Shiny App/Shiny app.R
+++ Preformance Shiny App/Shiny app.R
@@ -1,39 +1,69 @@
 
 library(shiny)
+install.packages(shinyalert)
+install.packages(tidyverse)
+library(shinyalert)
 
 # Define UI ----
 ui <- fluidPage(
-  titlePanel("Basic widgets"),
-  
-  fluidRow(
+  titlePanel("Fundraising Calculator"),
+  # Sidebar layout with a input and output definitions ----
+  sidebarLayout(
     
-    column(3,
-           radioButtons("Party", h3("What party is candidate A"),
-                        choices = list("Republican" = 1, "Democrat" = 2),selected = 1)),
- 
-    column(3, 
-           numericInput("LER", 
-                        h3("What percentage of the vote did the Democrat recieve for this race in the last election?"), 
-                        value = 1)),
-  
-  column(3, 
-         numericInput("CBF", 
-                      h3("How much money is candidate B expected to raise"), 
-                      value = 1)),
-  column(3,
-         submitButton("Submit"))))
-    
-    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Selector for choosing dataset ----
+      radioButtons("Party", "Political Party of Candidate", c("Republican"=1, 
+                                     "Democrat"=2), selected = NULL),
+      
+      # Input: Numeric entry for number of obs to view ----
+      numericInput("LER", 
+                h3("What percentage of the vote did the candidate of the same party as Candidate A recieve for this race in the last election?"), 
+                value = 50.00),
+    numericInput("CBF", 
+                h3("How much money is candidate B expected to raise"), 
+                value = 1),
+    numericInput("LE", 
+                h3("When was the last election?"), 
+                value = 2012),
+    numericInput("NPV", 
+                h3("What percentage of the vote is candidate a's party expected to get in congressional elections this year"), 
+                value = 50.00),
+    actionButton("submit","submit")
 
-  
+),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Verbatim text for data summary ----
+      verbatimTextOutput("value")
+      
+    )
+  )
+)
+
 
 # Define server logic ----
-server <- function(input, output) {
-  output$Party <- renderText({ 
-  paste()
+server <- function(input, output, session) {
+  observeEvent(input$submit, {
+    output$value<-renderPrint({
+    LE<-input$LE
+    Party<-input$Party
+    LER<-input$LER
+    NPV<-input$NPV
+    CBF<-input$CBF
+    NPVI<-(2*((NPV*.01)-.5))
+    PRPVI<-ifelse(Party==1,(a-NPVI),NPVI-a)
+    yp<-read.csv("https://raw.github.com/mb7068a/Final-Data-Project/master/Tables/yp.csv", sep="\t")
+    a<-yp%>%dplyr::filter(year==LE)
+    a<-a[-c(1,3,4)]
+    paste(a)
+    
+    
+     })
   })
-  
 }
-
 # Run the app ----
 shinyApp(ui = ui, server = server)
